0759291e55524a002f255b777cb876a8f3a86c78
Ali <alipudina>
2020-09-20 23:18:59 +0200

    build done

---
+++ build/precache-manifest.03a5fcf72e7a4d2c6c2e8301473f5a5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e911467b30f38c9bc5ef76788ac489c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c02b2b93bb5112d0b922",
+    "url": "/static/css/2.6b5d40a7.chunk.css"
+  },
+  {
+    "revision": "877a1afc0c57dc9f95d3",
+    "url": "/static/css/main.8abe715a.chunk.css"
+  },
+  {
+    "revision": "c02b2b93bb5112d0b922",
+    "url": "/static/js/2.3cc41171.chunk.js"
+  },
+  {
+    "revision": "3e533a1a18939928aa484d9457ec0229",
+    "url": "/static/js/2.3cc41171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877a1afc0c57dc9f95d3",
+    "url": "/static/js/main.d4ffffa3.chunk.js"
+  },
+  {
+    "revision": "7855e56724cc2dbbfd8e",
+    "url": "/static/js/runtime-main.be2e44d4.js"
+  }
+]);
